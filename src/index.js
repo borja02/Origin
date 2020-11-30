21a99b888f297eeac3898ff16462fc6873b705f6
borja02 <72219426+borja02@users.noreply.github.com>
2020-11-30 18:02:22 +0100

    d

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express()
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-const apijs = require('./api');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const api = require('./api');
+api.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+api.listen(port, () => {
     console.log("El servidor está inicializado en el puerto "+ port);
 });
 
