a27eb65987d0225f44fb6ca103aeeb94244ece6e
borja02 <72219426+borja02@users.noreply.github.com>
2020-11-30 18:23:34 +0100

    mn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('swagger.json');
+const swaggerDocument = require('./swagger.json');
 const api = require('./api');
 api.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
